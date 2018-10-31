d6e6f955cd1ccb1566e2c7113ba764e97e01d997
james7132 <james.siqi.liu@gmail.com>
2018-10-30 18:28:49 -0700

    Add BlockPipelines and requests -> aiohttp

--- tapioca/core/block_processors.py
+++ tapioca/core/block_processors.py
@@ -1,8 +1,17 @@
 import zlib
+import logging
 from abc import abstractmethod
+from tapioca.core.manifest import hash_block
+
+
+log = logging.getLogger(__name__)
 
 
 class BlockProcessor():
+    """An abstract class for intermediate processing stages within a
+    BlockPipeline. These processors may alter or change the provided blocks
+    before being written out to BlockSinks.
+    """
 
     @abstractmethod
     def process_block(self, block_hash, block):
@@ -10,15 +19,32 @@ class BlockProcessor():
 
 
 class GzipBlockProcessor(BlockProcessor):
+    """A BlockProcessor that gzip compresses blocks."""
 
     def __init__(self, level=9):
         self.level = level
 
     def process_block(self, block_hash, block):
+        log.info(f'Compressing block (gzip -{self.level}): "{block_hash.hex()}..."')
         return zlib.compress(block, self.level)
 
 
 class GunzipBlockProcessor(BlockProcessor):
+    """A BlockProcessor that decompresses gzip compressed blocks."""
 
     def process_block(self, block_hash, block):
+        log.info(f'Decompressing block (gzip): "{block_hash.hex()}..."')
         return zlib.decompress(block, self.level)
+
+
+class ValidateBlockProcessor(BlockProcessor):
+    """A BlockProcessor that validates whether the provided block matches the
+    assigned block hash.
+    """
+
+    def process_block(self, block_hash, block):
+        b_hash = hash_block(block)
+        if b_hash != block_hash:
+            log.error(f'Block hash mismatch: "{block_hash.hex()}" vs "{b_hash.hex()}"')
+            return None
+        return block
--- tapioca/core/block_sources.py
+++ tapioca/core/block_sources.py
@@ -1,6 +1,9 @@
 import os
-import requests
+import logging
 from abc import abstractmethod
+
+
+log = logging.getLogger(__name__)
 
 
 class BlockSource():
@@ -16,14 +19,22 @@ class BlockSource():
 
 
 class HttpBlockSource(BlockSource):
+    """A BlockSource that fetches blocks from a remote location."""
 
-    def __init__(self, prefix):
+    def __init__(self, prefix, session):
         self.prefix = prefix
+        self.session = session
 
-    def get_block(self, block_hash):
-        path = os.path.join(self.prefix, block_hash.hex())
-        r = requests.get(path)
-        return r.content
+    async def get_block(self, block_hash):
+        block_hex = block_hash.hex()
+        url = os.path.join(self.prefix, block_hex)
+        # TODO(james7132): Error handling
+        # TODO(james7132): Exponential fallback
+        log.info(f'Fetching block "{block_hex}" from {url}...')
+        async with self.session.get(url) as response:
+            block = await response.read()
+            log.info(f'Fetched block "{block_hex}" from {url}.')
+            return block
 
 
 class CachedBlockSource(BlockSource):
@@ -37,6 +48,7 @@ class CachedBlockSource(BlockSource):
     def get_block(self, block_hash):
         path = os.path.join(self.cache_dir, block_hash.hex())
         if os.path.exists(path):
+            log.info(f'Found block in cache: "{block_hex}"')
             with open(path, 'rb') as f:
                 return f.read()
         return self.base_source.get_block(block_hash)
