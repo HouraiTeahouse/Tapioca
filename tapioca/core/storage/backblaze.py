39885524a961e32503da368763d9713a1f152882
james7132 <contact@jamessliu.com>
2018-11-06 01:40:19 -0800

    Reduce number of HTTP calls to upload to Backblaze

--- tapioca/core/storage/backblaze.py
+++ tapioca/core/storage/backblaze.py
@@ -3,8 +3,8 @@ from tapioca.core.storage import BlockBucket
 class BackblazeBucket(BlockBucket):
 
     def __init__(self, bucket, prefix=None):
-        self.bucket = bucket
+        self.bucket = bucket.files
         self.prefix = prefix or ''
 
     def upload_file(self, path, data):
-        self.bucket.files.upload(file_name=path, contents=data)
+        self.bucket.upload(file_name=path, contents=data)
