c40d2dd9b4574c7c40e696b3198655d4775301fd
james7132 <james.siqi.liu@gmail.com>
2018-11-13 03:27:03 -0800

    Refactor for generality

--- cmd/tapioca_manifest_gen/tapioca.go
+++ cmd/tapioca_manifest_gen/tapioca.go
@@ -3,19 +3,22 @@ package main
 import (
 	"archive/zip"
 	"bufio"
+	"context"
 	"flag"
 	"io"
 	"log"
+  "os"
 	"strings"
-	"sync"
 
 	"github.com/HouraiTeahouse/Tapioca/pkg/blocks"
 	"github.com/HouraiTeahouse/Tapioca/pkg/manifests"
 	proto "github.com/golang/protobuf/proto"
+	"golang.org/x/sync/errgroup"
 )
 
 var (
 	input = flag.String("input", "", "Input zip file")
+	output = flag.String("output", "", "Input zip file")
 )
 
 func main() {
@@ -26,13 +29,70 @@ func main() {
 	}
 	defer rc.Close()
 
-	//processors := []block.BlockProcessor {
+	queue := make(chan *blocks.FileBlockData, 100)
 
-	//}
+	ctx, _ := context.WithCancel(context.Background())
+	ZipBlockSource(ctx, &rc.Reader, queue)
+	manifest, err := ProcessBlocks(ctx, queue)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	var wg sync.WaitGroup
+	manifestProto, err := manifest.ToProto()
+	if err != nil {
+		log.Fatalf("Manifest Proto error: %s", err)
+	}
+  if output == nil {
+    return
+  }
+
+  w, err := os.OpenFile(*output, os.O_WRONLY | os.O_CREATE | os.O_TRUNC, os.ModePerm)
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer w.Close()
+  if err := proto.MarshalText(w, manifestProto); err != nil {
+    log.Fatal(err)
+  }
+}
+
+func ProcessBlocks(ctx context.Context, in <-chan *blocks.FileBlockData) (*manifests.Manifest, error) {
+	var g *errgroup.Group
 	builder := manifests.ManifestBuilder{}
-	for _, file := range rc.File {
+	g, ctx = errgroup.WithContext(ctx)
+
+  loop:for {
+		select {
+		case file, ok := <-in:
+			if !ok {
+				break loop
+			}
+			f := file
+			g.Go(func() error {
+        return builder.AddFile(f.File).
+					AddBlock(f.BlockId, &manifests.ManifestBlock{
+						Hash: *f.Data.Hash(),
+						Size: f.Size,
+					})
+			})
+		case <-ctx.Done():
+			break
+		}
+	}
+	if err := g.Wait(); err != nil {
+		return nil, err
+	}
+	manifest, err := builder.Build()
+	if err != nil {
+		return nil, err
+	}
+	return manifest, nil
+}
+
+func ZipBlockSource(ctx context.Context, r *zip.Reader, out chan<- *blocks.FileBlockData) {
+	var g *errgroup.Group
+	g, ctx = errgroup.WithContext(ctx)
+	for _, file := range r.File {
 		if strings.HasSuffix(file.Name, "/") {
 			continue
 		}
@@ -40,55 +100,53 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		wg.Add(1)
-		go func(name string, f io.ReadCloser) {
+		name := file.Name
+		f := rc
+		g.Go(func() error {
 			defer f.Close()
-			defer wg.Done()
-			r := bufio.NewReader(f)
-			var blockId uint64 = 0
-			buf := make([]byte, 1024*1024)
-			manifestFile := builder.AddFile(name)
-			for {
-				n, err := io.ReadFull(r, buf)
-				block := blocks.CreateBlock(buf[:n])
-
-				wg.Add(1)
-				go func(id uint64) {
-					defer wg.Done()
-
-					blockErr := manifestFile.AddBlock(id, &manifests.ManifestBlock{
-						Hash: *block.Hash(),
-						Size: block.Size(),
-					})
-					if blockErr != nil {
-						log.Fatal(blockErr)
-					}
-				}(blockId)
-
-				if err != nil {
-					if err != io.EOF && err != io.ErrUnexpectedEOF {
-						log.Fatal(err)
-					}
-					return
-				}
-				blockId++
-			}
-		}(file.Name, rc)
+			return ProcessReader(ctx, name, f, out)
+		})
 	}
-	wg.Wait()
-	manifestProto, err := builder.BuildProto()
-	if err != nil {
-		log.Fatalf("Manifest Proto error: %s", err)
-	}
-	log.Println(proto.MarshalTextString(manifestProto))
+	go func() {
+		defer close(out)
+		g.Wait()
+	}()
 }
 
-//func RunProcessors(b *FileBlockData, p []blocks.BlockProcessor) error {
-//for _, processor := range p {
-//b, err := processor(b)
-//if err != nil {
-//return err
-//}
-//return err
-//}
-//}
+func ProcessReader(parent context.Context, name string, f io.Reader,
+	out chan<- *blocks.FileBlockData) error {
+	r := bufio.NewReader(f)
+	var blockId uint64 = 0
+	var offset uint64 = 0
+	g, ctx := errgroup.WithContext(parent)
+	buf := make([]byte, 1024*1024)
+	for {
+		n, err := io.ReadFull(r, buf)
+		block := blocks.CreateBlock(buf[:n])
+
+		fileBlock := &blocks.FileBlockData{
+			File:    name,
+			Offset:  offset,
+			BlockId: blockId,
+			Size:    block.Size(),
+			Data:    block,
+		}
+		g.Go(func() error {
+			select {
+			case out <- fileBlock:
+			case <-ctx.Done():
+			}
+			return nil
+		})
+
+		if err != nil {
+			if err != io.EOF && err != io.ErrUnexpectedEOF {
+				return err
+			}
+			return nil
+		}
+		blockId++
+		offset += block.Size()
+	}
+	return g.Wait()
+}
